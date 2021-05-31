6052621c6f9441da87d15508669f69935be6ac56
yoann-dufresne <yoann.dufresne0@gmail.com>
2021-05-31 19:23:09 +0200

    initial commit

---
+++ level.hpp
@@ -0,0 +1,34 @@
+
+// uint8_t bin_levels[] = {
+// //		Nb levels
+// 	2,
+
+// //    Laby, nb faces and faces list
+// 	'L',  1, 4,
+// //    Internal walls                      external walls
+//   0b10001001, 0b00010010, 0b10010100, 0xFF, 0xFF,
+// //    Hero coordinates
+//   4, 0, 0,
+// // Objects
+//   2,
+//   'w', 4, 3, 3,
+//   'l', 4, 3, 2,
+
+// //    Laby, nb faces and faces list
+// 	'L',  1, 4,
+// //    Internal walls                      external walls
+//   0b10001001, 0b00010010, 0b10010100, 0xFF, 0xFF,
+// //    Hero coordinates
+//   4, 1, 1,
+// // Objects
+//   3,
+//   'w', 4, 0, 3,
+//   'l', 4, 0, 1,
+//   'l', 4, 3, 3,
+// };
+
+
+uint8_t bin_levels[] = {3,
+	76,16,66,6,128,72,255,255,1,119,77,
+	76,16,64,137,18,148,255,255,2,101,78,119,79,
+	76,16,69,137,18,148,255,255,3,101,65,119,67,101,79};
---
+++ write_prog.ino
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include <EEPROM.h>
+
+#include "level.hpp"
+
+
+
+void setup() {
+	Serial.begin(115200);
+
+  Serial.print("Start writing EEPROM of size ");Serial.println(sizeof(bin_levels));
+	for (int i=0 ; i<sizeof(bin_levels) ; i++)
+		EEPROM.write(i, bin_levels[i]);
+  Serial.println("Program writed");
+}
+
+void loop() {
+	delay(1000);
+}
+
