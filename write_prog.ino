c2e27646547e9aa7d0df5eb2bfee4bed323ff725
yoann-dufresne <yoann.dufresne0@gmail.com>
2021-06-05 17:11:59 +0200

    typo

--- write_prog.ino
+++ write_prog.ino
@@ -11,7 +11,7 @@ void setup() {
   Serial.print("Start writing EEPROM of size ");Serial.println(sizeof(bin_levels));
 	for (int i=0 ; i<sizeof(bin_levels) ; i++)
 		EEPROM.write(i, bin_levels[i]);
-  Serial.println("Program writed");
+  Serial.println("Program written");
 }
 
 void loop() {
